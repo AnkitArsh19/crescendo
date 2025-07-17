18ad3448f25be4eba03e68427eec1031b9596af6
Ankit Arsh <ankitarsh19@gmail.com>
2025-07-17 16:29:51 +0530

    First Commit

---
+++ crescendo-backend/src/main/java/com/crescendo/CrescendoApplication.java
@@ -0,0 +1,13 @@
+package com.crescendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrescendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrescendoApplication.class, args);
+	}
+
+}
