d1e1569425b6fd79c2b71dcdfc5b3db0c0e34f54
Ankit Arsh <ankitarsh19@gmail.com>
2026-07-23 17:54:39 +0100

    fix: Revert incorrect @EnableJpaRepositories on main class

--- crescendo-backend/src/main/java/com/crescendo/CrescendoApplication.java
+++ crescendo-backend/src/main/java/com/crescendo/CrescendoApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 @SpringBootApplication
 @EnableAsync
-@EnableJpaRepositories
 // Application entry point.
 public class CrescendoApplication {
 
